e82f7f22914ae54b6e7c99384530da79de5272b2
acceleration3 <acceleration23@gmail.com>
2020-08-10 00:35:38 +0100

    Fixed SSL erros some people were getting.

--- Setup.ps1
+++ Setup.ps1
@@ -8,8 +8,10 @@ If (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 
 function Write-HostCenter { param($Message) Write-Host ("{0}{1}" -f (' ' * (([Math]::Max(0, $Host.UI.RawUI.BufferSize.Width / 2) - [Math]::Floor($Message.Length / 2)))), $Message) }
 
+
 Start-Transcript -Path "Log.txt"
 Set-Location -Path $PSScriptRoot
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
 clear
 
